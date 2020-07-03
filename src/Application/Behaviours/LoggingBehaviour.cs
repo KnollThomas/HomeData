d21a0ec9897c721279020346fae81734860741c7
Thomas Knoll <41270522+Heidelbeerstrudl@users.noreply.github.com>
2020-07-03 13:26:08 +0200

    added logging behaviour and validation exception

---
+++ src/Application/Behaviours/LoggingBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using MediatR.Pipeline;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Behaviours
+{
+    public class LoggingBehaviour<TRequest> : IRequestPreProcessor<TRequest>
+    {
+        public Task Process(TRequest request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Application/Exceptions/ValidationException.cs
@@ -0,0 +1,36 @@
+﻿using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Application.Exceptions
+{
+    public class ValidationException : Exception
+    {
+        public ValidationException()
+            :base("One or more Validation Errors have occured")
+        {
+            Errors = new Dictionary<string, string[]>();
+        }
+
+        public ValidationException(IEnumerable<ValidationFailure> failures)
+            : this()
+        {
+            var failureGroups = failures
+                .GroupBy(e => e.PropertyName, e => e.ErrorMessage);
+
+            foreach (var failureGroup in failureGroups)
+            {
+                var propertyName = failureGroup.Key;
+                var propertyFailures = failureGroup.ToArray();
+
+                Errors.Add(propertyName, propertyFailures);
+            }
+        }
+
+
+        public Dictionary<string, string[]> Errors { get; }
+    }
+}
